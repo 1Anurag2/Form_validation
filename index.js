2826f1ac58c5d3edd76844ceabda508e5cea184c
Anurag Verma <147230214+1Anurag2@users.noreply.github.com>
2025-04-11 01:39:17 +0530

    FormValidation

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ dotenv.config();
 const app = express();
 
 import formRouter from "./router/formRouter.js";
+import { connectDB } from "./config/dbconncet.js";
 
 const port = process.env.PORT;
 
@@ -12,6 +13,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
 
+//Database
+connectDB()
+
+//Router
 app.use('/',formRouter)
 
 app.listen(port, () => {
